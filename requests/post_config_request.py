925dd51bb35e6445efe1f316f6d5093f0100ab64
sabbey-uw <sabbey@uw.co.uk>
2021-11-20 16:08:46 +0000

    database

--- requests/post_config_request.py
+++ requests/post_config_request.py
@@ -11,7 +11,7 @@ class PostConfigRequest(SlashCommandRequest):
 
     def handle_channel(self, channel):
         config_string = "Perkeles are enabled in this channel:\n" \
-                        "They will come after " + channel.hours_until_perkele + " hours.\n" \
+                        "They will come after " + str(channel.hours_until_perkele) + " hours.\n" \
                         "Expected civ hours are from 8:00 - 18:00 on weekdays"
         client.chat_postMessage(channel=channel.id, text=config_string)
         return Response(), 200
