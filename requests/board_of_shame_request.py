73adee002a9d810beba375fbe5ed9191438db25b
sabbey-uw <sabbey@uw.co.uk>
2021-11-29 10:07:13 +0000

    user name attempt

--- requests/board_of_shame_request.py
+++ requests/board_of_shame_request.py
@@ -14,28 +14,29 @@ class BoardOfShameRequest(SlashCommandRequest):
 def send_board_of_shame(client, channel_id, session):
     perkele_counts = session.query(PerkeleCount).filter(PerkeleCount.channel_id == channel_id).all()
     perkele_counts.sort(key=lambda x: x.perkele_count, reverse=True)
-    board_of_shame = build_board_of_shame(perkele_counts, client)
+    users_list = client.users_list().get("members")
+    board_of_shame = build_board_of_shame(perkele_counts, users_list)
     client.chat_postMessage(channel=channel_id, text=board_of_shame)
 
 
-def build_board_of_shame(perkele_counts, client):
+def build_board_of_shame(perkele_counts, users_list):
     heading_1 = "Offender"
     heading_2 = "Offences"
     divider = "----------------------------"
     heading_section = "``` %s \n %s \n %s       %s \n ========================= \n" % (
         SHAME_BOARD_TITLE, divider, heading_1, heading_2)
-    middle_sections = map(lambda x: build_shame_row(x, client),
+    middle_sections = map(lambda x: build_shame_row(x, users_list),
                           perkele_counts)
     table_section = '\n'.join(middle_sections)
     footer_section = "\n %s ```" % divider
     return heading_section + table_section + footer_section
 
 
-def build_shame_row(perkele_count, client):
+def build_shame_row(perkele_count, users_list):
     user_id = perkele_count.user_id
-    user_info = client.users_info(user_id)
-    if user_info.get('ok'):
-        user_name = user_info.get('user').get('name')
+    user_info = next((x for x in users_list if x.get('id') == user_id), None)
+    if user_info is not None:
+        user_name = user_info.get('name')
     else:
         user_name = "<@%s>" % user_id
     return "%s     %i    " % (user_name, perkele_count.perkele_count)
