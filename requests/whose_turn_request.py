d7c391206c85c986bc7f9af46670834de3862c91
sabbey-uw <sabbey@uw.co.uk>
2022-02-23 11:49:07 +0000

    Adding turn time

--- requests/whose_turn_request.py
+++ requests/whose_turn_request.py
@@ -7,14 +7,14 @@ class WhoseTurn(Request):
     def handle_session(self):
         turns = self.session.query(TurnNotification).all()
         strings = [make_turn_desc(t, self.client) for t in turns]
-        return '\n'.join(strings)
+        return ' '.join(strings)
 
 
 def make_turn_desc(turn, client):
     users_list = client.users_list().get("members")
     name = get_user_name(turn.user_id, users_list)
     channel = get_channel_name(turn.channel_id, client)
-    return f"In {channel} it is {name}'s turn."
+    return f"In {channel} it was {name}'s turn at {turn.timestamp.strftime('%H:%M:%S %d/%m/%Y')}."
 
 
 def get_channel_name(channel_id, client):
@@ -22,4 +22,4 @@ def get_channel_name(channel_id, client):
     if channel is not None:
         return channel.get('channel').get('name')
     else:
-        return 'Unkown channel'
+        return 'Unknown channel'
