b5ba8a641496ea20ede20e9d9501ca11ff66a7e6
sabbey-uw <sabbey@uw.co.uk>
2022-03-31 08:37:02 +0100

    Adding shit BST support

--- manual_config.py
+++ manual_config.py
@@ -2,7 +2,10 @@ import datetime
 
 SHAME_BOARD_TITLE = 'Board of Shame'
 
-START_OF_CIV_DAY = datetime.time(8, 0, 0)
-END_OF_CIV_DAY = datetime.time(18, 0, 0)
+BST_ADJUSTMENT = 1
+
+START_OF_CIV_DAY = datetime.time(8 - BST_ADJUSTMENT, 0, 0)
+END_OF_CIV_DAY = datetime.time(18 - BST_ADJUSTMENT, 0, 0)
+
 
 INACTIVE_ON_WEEKEND = True
